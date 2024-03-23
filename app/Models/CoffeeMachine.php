5eee53e94ddfa9ef613948bb23ee9c776a64a3b7
FaboBorgesLima <fabo.borges.lima@gmail.com>
2024-03-23 17:42:13 -0300

    adding makeTea to coffee machine

--- app/Models/CoffeeMachine.php
+++ app/Models/CoffeeMachine.php
@@ -7,6 +7,11 @@ class CoffeeMachine
     public static function makeCoffee(int $amount): string
     {
 
-        return "$amount ml de cafe";
+        return "$amount ml of coffee";
+    }
+    public static function makeTea(int $amount): string
+    {
+
+        return "im not a tea pot";
     }
 }
